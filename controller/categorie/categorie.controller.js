81d3dfa8ebaa5c63c7f0beb29d38864048fc9788
Nishith <nishith@Shreyass-MacBook-Air.local>
2023-04-29 13:26:10 +0530

    Changes to the week

--- controller/categorie/categorie.controller.js
+++ controller/categorie/categorie.controller.js
@@ -282,7 +282,7 @@ exports.weekGraph = async(req,res,next)=>{
       }
     }, {
       '$group': {
-        '_id': { '$ceil': { '$divide': [ { '$dayOfMonth': "$createdAt" }, 7 ] } }, 
+        '_id': { '$week':'$createdAt' }, 
         'total': {
           '$sum': '$main.amount'
         }
